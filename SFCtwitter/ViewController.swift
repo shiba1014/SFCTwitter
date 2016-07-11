43c3539fd903498a5406b3b6d9e08ae3b84e55e2
Paul McCartney <shiba@paul-mccartney.local>
2016-07-11 14:57:57 +0900

    Initial Commit

---
+++ SFCtwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFCtwitter
+//
+//  Created by Paul McCartney on 2016/07/11.
+//  Copyright © 2016年 shiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
